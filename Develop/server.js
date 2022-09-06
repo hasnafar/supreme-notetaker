339185ccc89d4e951c59e9d7da5981a35b3d3345
hasnafar <hasnafarrukh@gmail.com>
2022-09-06 12:46:18 -0400

    Add get and post requests in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,35 @@ const notes=require('./db/db.json');
 const PORT=8080;
 
 const app=express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
+    console.log(notes.length);
+})
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'/public/index.html'));
+})
+
+app.get('/notes',function(req,res){
+    res.sendFile(path.join(__dirname,'/public/notes.html'));
+})
+
+app.get('/api/notes',function(req,res){
+    res.json(notes);
+})
+
+app.post('/api/notes',function(req,res){
+
+    const note = {
+        id: notes.length+1,
+        title: req.body.title,
+        text: req.body.text,
+    }
+    
+    notes.push(note);
+    res.send(notes);
+}) 
